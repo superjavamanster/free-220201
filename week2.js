0019546456ce4683f93ecc0327f090e2e55ec181
superjavamanster <superjavamanster@gmail.com>
2022-02-01 21:27:00 +0800

    差異

--- week2.js
+++ week2.js
@@ -5,36 +5,45 @@ const api_path = 'samwnkyat';
 const app = createApp({
     data() {
         return {
-            products: [],
+          apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+          apiPath: 'hexschoolvue',
+          products: [],
+          tempProduct: {},
         }
-    },
-    methods: {
-        checkLogin() {
-            const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-            axios.defaults.headers.common['Authorization'] = token;
-
-
-            const url = `${site}/api/user/check`;
-            axios.post(url)
-                .then(res => {
-                    console.log(res);
-                    this.getProducts();
-                });
+      },
+      methods: {
+        checkAdmin() {
+          const url = `${this.apiUrl}/api/user/check`;
+          axios.post(url)
+            .then(() => {
+              this.getData();
+            })
+            .catch((err) => {
+              alert(err.data.message)
+              window.location = 'index.html';
+            })
         },
-        getProducts() {
-            const url = `${site}/api/${api_path}/admin/products/all`;
-            axios.get(url)
-                .then(res => {
-                    console.log(res);
-                    this.products = res.data.products;
-                });
+        getData() {
+          const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+          axios.get(url)
+            .then((response) => {
+              this.products = response.data.products;
+            })
+            .catch((err) => {
+              alert(err.data.message);
+            })
+        },
+        openProduct(item) {
+          this.tempProduct = item;
         }
-    },
-    mounted() {
-        this.checkLogin();
-
-        //axios.post(url)
-    }
+      },
+      mounted() {
+        // 取出 Token
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        axios.defaults.headers.common.Authorization = token;
+    
+        this.checkAdmin()
+      }
 });
 
 app.mount('#app');
